c9af769d2b98f578be6cf6e4cbc18d7ba9340fc9
Han1803740 <25021751@vnu.edu.vn>
2026-05-14 22:49:55 +0700

    update Login

--- src/main/java/Client/controller/LoginController.java
+++ src/main/java/Client/controller/LoginController.java
@@ -1,3 +1,7 @@
+import Client.controller.SceneUtil;
+import Client.networking.NetworkClient;
+import Server.controller.UserApiController;
+import Server.controller.responseObjects.ApiResponse;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,6 +13,9 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+import static Client.controller.SceneUtil.showAlert;
+import static Client.controller.SceneUtil.switchToScene;
+
 public class LoginController {
     @FXML
     private Button btnLogin;
@@ -17,26 +24,46 @@ public class LoginController {
     @FXML
     private TextField txtUserName;
 
+    public static class LoginRequest {
+        String username;
+        String password;
+
+        public LoginRequest(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+    }
+
     @FXML
     void handleLogin(ActionEvent event) {
         String user = txtUserName.getText();
         String pass = txtPassWord.getText();
         if (user.isEmpty() || pass.isEmpty()) {
-            System.out.println("Vui lòng nập đầy đủ thông tin");
+            SceneUtil.showAlert("Thông báo ", "Vui lòng nập đầy đủ thông tin");
             return;
         }
-        try (Socket socket = new Socket("localhost", 1234);
-             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-             DataInputStream in = new DataInputStream(socket.getInputStream())) {
-            out.writeUTF("LOGIN | " + user + "|" + pass);
-            out.flush();
-            if (in.readUTF().equalsIgnoreCase("SUCCESS")) {
-                System.out.println("Đăng nhập thành công");
-            } else {
-                System.out.println("Tài khoản hoặc mật khẩu không đúng vui lòng thử lại");
+        try {
+            LoginRequest data = new LoginRequest(user, pass);
+            ApiResponse res = NetworkClient.getInstance().post("/login", data);
+            if ( res !=null && res.getStatus() == 200) {
+                Object userObj = res.getData();
+                if (userObj != null) {
+                    String userData = userObj.toString().toUpperCase();
+                    if (userData.contains("ADMIN")) {
+                        SceneUtil.switchToScene("/Client/views/Admin.fxml", "Quản Trị Viên");
+                    } else if (userData.contains("SELLER")) {
+                        SceneUtil.switchToScene("/Client/views/Sellerview.fxml", "Người Bán");
+                    } else {
+                        SceneUtil.switchToScene("/Client/views/Userview.fxml", "Người Dùng");
+                    }
+                }else {
+                    String msg = (res != null) ? res.getMessage() : "Lỗi kết nối";
+                    showAlert("Đăng nhập thất bạn ", msg);
+                }
             }
-        } catch (IOException e) {
-            System.out.println("Không kết nối  được ");
+        } catch (Exception e) {
+            e.printStackTrace();
+            SceneUtil.showAlert("Lỗi kết nối ", "Không thể kết nối tới Server");
         }
     }
 }
