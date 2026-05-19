be1dfb31dc3d514a8521f2e4f0df5b11eb370081
LeThiHuyenMy <25021900@uet.edu.vn>
2026-05-19 12:05:31 +0700

    autobidding

--- src/main/java/Server/service/auction/AutoBidConfigService.java
+++ src/main/java/Server/service/auction/AutoBidConfigService.java
@@ -2,10 +2,81 @@ package Server.service.auction;
 
 import Server.model.auction.AutoBidConfig;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.concurrent.ConcurrentHashMap;
-
 public class AutoBidConfigService {
+
     private final Map<Integer, PriorityQueue<AutoBidConfig>> autoBidmaps = new ConcurrentHashMap<>();
+    private final BiddingService biddingService;
+
+    public  AutoBidConfigService (BiddingService biddingService){
+        this.biddingService = biddingService;
+    }
+
+    public synchronized void registerAutoBid(int auctionId, int userId, double maxBid, double increment){
+        AutoBidConfig autoBid = new AutoBidConfig(auctionId, userId, maxBid, increment);
+
+        autoBidmaps.putIfAbsent(auctionId, new PriorityQueue<>());
+
+        autoBidmaps.get(auctionId).add(autoBid);
+
+    }
+    public synchronized void triggerAutoBidding(int auctionId){
+        PriorityQueue<AutoBidConfig> queue = autoBidmaps.get(auctionId);
+
+        if (queue == null || queue.isEmpty()) return;
+
+        double currentPrice = getCurrentPriceFromDB(auctionId);
+
+        AutoBidConfig top1Config = queue.poll();
+        AutoBidConfig top2Config = queue.poll(); //có thể null
+
+        double max2 = top2Config.getMaxBid();
+        double inc1 = top1Config.getIncrement();
+        double inc2 = top2Config.getIncrement();
+        double totalIncrement = inc1 + inc2;
+        double buggetLeftB = max2 - currentPrice;
+        double loop = buggetLeftB / totalIncrement;
+
+        double finalPrice = currentPrice;
+        int winnerBotId = -1;
+
+        if (top1Config.getMaxBid()< finalPrice) return;
+
+        else if (top2Config == null) {
+            winnerBotId = top1Config.getUserId();
+            finalPrice = Math.min(currentPrice + inc1, top1Config.getMaxBid());
+            queue.add(top1Config);
+        }
+        else if (top1Config.getMaxBid()==top2Config.getMaxBid()){
+            finalPrice = top1Config.getMaxBid();
+            if (loop*totalIncrement == buggetLeftB){
+                winnerBotId = top2Config.getUserId();
+                queue.add(top2Config);
+            }
+            else {
+                winnerBotId = top1Config.getUserId();
+                queue.add(top1Config);
+            }
+        }
+        else{
+            finalPrice = currentPrice + loop * totalIncrement + inc1;
+            winnerBotId = top1Config.getUserId();
+            queue.add(top1Config);
+        }
+        if (winnerBotId != -1 && finalPrice > currentPrice){
+            try {
+                biddingService.placeBidInternal(winnerBotId, auctionId, finalPrice);
+                System.out.println("BotWinner: "  + winnerBotId  + " || FinalPrice: " + finalPrice);
+            }catch (Exception e){
+                System.out.println("Lỗi cập nhật giá" + e.getMessage());
+            }
+        }
+    }
+    private double getCurrentPriceFromDB(int auctionId){
+        return biddingService.getCurrentPrice(auctionId);
+
+    }
 }
--- src/main/java/Server/service/auction/BiddingService.java
+++ src/main/java/Server/service/auction/BiddingService.java
@@ -18,12 +18,14 @@ public class BiddingService {
     private final UserDAO userDAO;
     private final AuctionDAO auctionDAO;
     private final BidDAO bidDAO;
+    private final AutoBidConfigService autoBidConfigService;
 
-    public BiddingService(DataSource ds, UserDAO u, AuctionDAO a, BidDAO b) {
+    public BiddingService(DataSource ds, UserDAO u, AuctionDAO a, BidDAO b, AutoBidConfigService abs) {
         this.dataSource = ds;
         this.userDAO = u;
         this.auctionDAO = a;
         this.bidDAO = b;
+        this.autoBidConfigService = abs;
     }
 
     public void placeBid(int userId, int auctionId, double amount) {
@@ -69,6 +71,63 @@ public class BiddingService {
                     bidDAO.updateEndtime(conn, auctionId, newEndtime);
                 }
 
+                conn.commit();
+                autoBidConfigService.triggerAutoBidding(auctionId);
+
+            } catch (Exception e) {
+                conn.rollback();
+                throw new RuntimeException(e.getMessage(), e);
+            }
+
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //Dành cho bot
+    public void placeBidInternal(int userId, int auctionId, double price) {
+        try (Connection conn = dataSource.getConnection()) {
+            conn.setAutoCommit(false);
+            try {
+                // Lock the auction row for this transaction to prevent concurrent updates.
+                Auction auction = auctionDAO.findByIdForUpdate(conn, auctionId);
+                if (auction == null) throw new RuntimeException("Auction not found");
+
+                if (!"RUNNING".equals(auction.getStatus())) {
+                    throw new RuntimeException("Auction is not running");
+                }
+
+                if (auction.getEndTime().isBefore(LocalDateTime.now())) {
+                    throw new RuntimeException("Auction has ended");
+                }
+
+                if (price <= auction.getCurrentPrice()) {
+                    throw new RuntimeException("Bid must be higher than current price of " + auction.getCurrentPrice());
+                }
+
+                // Read the bidder's current balance inside the same transaction.
+                User user = userDAO.findById(conn, userId);
+                if (user == null) throw new RuntimeException("User not found");
+
+                if (user.getBalance() < price) {
+                    throw new RuntimeException("Insufficient balance for Bot ");
+                }
+
+                // All three writes share the same connection and will commit or rollback together.
+                userDAO.updateBalance(conn, userId, user.getBalance() - price);
+                auctionDAO.updateCurrentPrice(conn, auctionId, price);
+                bidDAO.create(conn, userId, auctionId, price);
+
+                //snipping
+                LocalDateTime now = LocalDateTime.now();
+                long finalminutes = 5;
+                long extendminutes = 3;
+                if (now.isAfter(auction.getEndTime().minusMinutes(finalminutes)) && now.isBefore(auction.getEndTime())) {
+                    LocalDateTime newEndtime = auction.getEndTime().plusMinutes(extendminutes);
+                    bidDAO.updateEndtime(conn, auctionId, newEndtime);
+                }
+
 
                 conn.commit();
 
@@ -83,8 +142,23 @@ public class BiddingService {
             throw new RuntimeException(e);
         }
     }
-
     public List<Bid> getBidHistory(int auctionId) {
         return bidDAO.getBidsByAuction(auctionId);
     }
+
+    public double getCurrentPrice(int auctionId){
+        try {
+            Auction auction = auctionDAO.findById(auctionId);
+            if (auction == null){
+                throw  new RuntimeException("Không tìm thấy phiên đấu giá");
+            }
+            return auction.getCurrentPrice();
+        }catch (RuntimeException e){
+            System.out.println("Lỗi: " + e.getMessage());
+            throw e;
+        }
+    }
+
+
+
 }
